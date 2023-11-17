c1c2ed3b76c2d965e695f2fd5396879361be0c3f
Katriceb <146128660+Katriceb@users.noreply.github.com>
2023-11-17 13:47:35 -0500

    Add files via upload

---
+++ StarShipCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+//function StarShipCard() {
+  const StarShipCard =({starship})=> {
+
+   return (
+    <div className ="starship-card">
+       <h1>{starship.name}</h1>
+        </div>
+  )
+};
+
+export default StarShipCard;
